2bc772fe41da169477b27688e47c04bedf4aaf64
Wiraa <wira007911@gmail.com>
2025-03-05 21:26:00 +0800

    03

---
+++ 03-writing-test/index.test.js
@@ -0,0 +1,14 @@
+import test from 'node:test';
+import assert from 'node:assert';
+import { sum } from './index.js';
+
+test('Mengembalikan hasil penjumlahan dengan benar', () => {
+  assert.strictEqual(sum(2, 3), 5);
+  assert.strictEqual(sum(-1, 1), 0);
+  assert.strictEqual(sum(0, 0), 0);
+});
+
+test('Menghandle angka desimal dengan benar', () => {
+  assert.strictEqual(sum(2.5, 2.5), 5);
+  assert.strictEqual(sum(0.1, 0.2), 0.3);
+});
